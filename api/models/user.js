d58314de60762a61db0a0f1d84b9e268892077ff
ABHILASH N <abhilashgowda.1996@gmail.com>
2018-07-17 09:44:08 +0530

    forgot password

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const passportLocalMongoose = require("passport-local-mongoose");
+//const passportLocalMongoose = require("passport-local-mongoose");
 const dotenv = require("dotenv");
+const bcrypt = require('bcrypt'),
+Schema = mongoose.Schema;
 
 var minuteFromNow = function(){
     var d = new Date();
@@ -35,10 +37,21 @@ const userSchema = mongoose.Schema({
     experience: { type: String, required: false },
     creation_time : { type : String, default: minuteFromNow },
     lastLogin :{ type : String, default: minuteFromNow },
-    resetPasswordToken:{ type: String },
-    resetPasswordExpires:{type:Date}
-
+    reset_password_token: {
+      type: String
+    },
+    reset_password_expires: {
+      type: Date
+    }
 });
+
+
+userSchema.methods.comparePassword = function(password) {
+  return bcrypt.compareSync(password, this.hash_password);
+};
+
+
+//mongoose.model('User', userSchema);
 
 module.exports = mongoose.model('User', userSchema);
 
--- api/routes/userroutes.js
+++ api/routes/userroutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const body = require("body-parser");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -8,7 +9,11 @@ const async = require("async");
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
 const dotenv = require("dotenv");
+//hbs = require('nodemailer-express-handlebars'),
+email = process.env.MAILER_EMAIL_ID || 'noreply.iclbmsce@gmail.com',
+pass = process.env.MAILER_PASSWORD || 'bms-icl123',
 
+module.exports = router;
 require('dotenv').config()
 jwt_key: process.env.JWT_KEY
 mail_key :process.env.MAIL_KEY
@@ -23,31 +28,82 @@ var minuteFromNow = function(){
  return n;
 };
 
+//udemy
+/*
+router.put("/:userId", (req, res, next) => {
+  const id = req.params.userId;
+ 
+ User.findOneAndUpdate({
+  _id: id }, {
+   $set:{
+     fname: req.body.f_name
+     }
+   },{
+     returnOriginal: false
+   })
+ .then(result =>{
+   console.log(result);
+   res.status(200).json({
+       message: 'user details updated',
+       request: {
+           type: 'GET',
+           url: 'http://localhost:3000/user/'+ id
+       }
+   });
+ })
+ .catch(err => {
+   console.log(err);
+   res.status(500).json({
+     error: err
+   });
+ });
+});
+
+*/
+
+
+
+router.put("/:userId", (req, res, next) => {
+  const id = req.params.userId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  User.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result =>{
+      console.log(result);
+      res.status(200).json({
+          message: 'user details updated',
+          request: {
+              type: 'GET',
+              url: 'http://localhost:3000/user/'+ id
+          }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+
+
+
+
 
 router.get("/", (req, res, next) => {
   User.find()
-  .select("_id username f_name m_name l_name phone weight height medical_con pain_areas medications experience creation_time lastLogin")
+ .select("_id email username f_name m_name l_name phone weight height medical_con pain_areas medications experience creation_time lastLogin")
   .exec()
   .then(docs => {
     const response = {
       count: docs.length,
       user: docs.map(doc => {
         return {
-          _id: doc._id,
-          username: doc.username,
-          f_name: doc.f_name,
-          m_name:doc.m_name,
-          l_name: doc.l_name,
-          phone : doc.phone,
-          weight: req.body.weight,
-          height: req.body.height,
-          phone: req.body.phone,
-          medical_con: req.body.medical_con,
-          pain_areas: req.body.pain_areas,
-          medications: req.body.medications,
-          experience: req.body.experience,
-          creation_time: doc.creation_time,
-          lastLogin : doc.lastLogin,
+          user_details: doc,
           request: {
             type: "GET",
             url: "http://localhost:3000/user/"+ doc._id   
@@ -66,6 +122,38 @@ router.get("/", (req, res, next) => {
     });
   });
 });
+
+
+// FIND BY MAIL
+/*
+router.get("/:userEmail", (req, res, next) => {
+  const email = req.params.userEmail;
+  User.find({ email: req.body.email })
+    .select('_id email username f_name m_name l_name phone weight height medical_con pain_areas medications experience creation_time lastLogin')
+    .exec()
+    .then(doc => {
+      console.log("From database", doc);
+      if (doc) {
+        res.status(200).json({
+            user_details: doc,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/user'
+            }
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: "No user found for provided ID" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+*/
 
 
 
@@ -101,35 +189,6 @@ router.get("/:userId", (req, res, next) => {
 
 
 
-
-router.put("/:userId", (req, res, next) => {
-  const id = req.params.userId;
-  const updateOps = {};
-  for (const ops of req.body) {
-    updateOps[ops.propName] = ops.value;
-  }
-  User.update({ _id: id }, { $set: updateOps })
-    .exec()
-    .then(result =>{
-      console.log(result);
-      res.status(200).json({
-          message: 'user details updated',
-          request: {
-              type: 'GET',
-              url: 'http://localhost:3000/user/'+ id
-          }
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-});
-
-
-
 router.post("/signup", (req, res, next) => {
   User.find({ email: req.body.email })
     .exec()
@@ -139,16 +198,11 @@ router.post("/signup", (req, res, next) => {
           message: "Mail exists"
         });
       } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err
-            });
-          } else {
+         
             const user = new User({
               _id: new mongoose.Types.ObjectId(),
               email: req.body.email,
-              password: hash,
+             // password: hash,
               phone:req.body.phone,
               username:req.body.username,
               f_name: req.body.f_name,
@@ -163,8 +217,8 @@ router.post("/signup", (req, res, next) => {
               experience: req.body.experience
               
             });
-            user
-              .save()
+            user.password = bcrypt.hashSync(req.body.password, 10);
+              user.save()
               .then(result => {
                 console.log(result),
                 User.update({email:req.body.email},{$set : { creation_time : minuteFromNow()}},function(err) {
@@ -184,8 +238,8 @@ router.post("/signup", (req, res, next) => {
                 });
               });
           }
-        });
-      }
+        
+      
     });
 });
 
@@ -208,7 +262,7 @@ router.post("/login", (req, res, next) => {
 
 
         if (result) {
-          const token = jwt.sign
+          const success_token = jwt.sign
           (
             {
               email: user[0].email,
@@ -233,7 +287,7 @@ router.post("/login", (req, res, next) => {
                } )
           return res.status(200).json({
             message: "Auth successful",
-           token: token
+           success_token: success_token
           });
         }
        }
@@ -250,6 +304,14 @@ router.post("/login", (req, res, next) => {
     });
 });
 
+
+router.get('/logout', (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
 /*
 router.get('/logout', function(req, res) {
   req.token.destroy();
@@ -289,152 +351,161 @@ router.delete("/:userId", (req, res, next) => {
       });
     });
 });
-/*
 
 
-router.put("/:userId", (req, res, next) => {
-  User.findByIdAndUpdate({ _id: req.params.id }, req.body)
-  .then(function(){
-    User.findOne({_id:req.params.id})
-    .then(function(user)
-    {
-      res.send(user);
-    });
-  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const smtpTransport = nodemailer.createTransport({
+  service: process.env.MAILER_SERVICE_PROVIDER || 'Gmail',
+  auth: {
+    user: email,
+    pass: pass
+  }
 });
-    
+
+/*
+const handlebarsOptions = {
+  viewEngine: 'handlebars',
+  viewPath: path.resolve('./api/templates/'),
+  extName: '.html'
+};
+
+smtpTransport.use('compile', hbs(handlebarsOptions));
+
+
+
 */
 
 
-
-
-
-
-
-
-
-
-
-
-// forgot password
-
-
-router.post('/forgot', function(req, res, next) {
+router.post("/forgot", (req, res, next) => {
   async.waterfall([
     function(done) {
-      crypto.randomBytes(20, function(err, buf) {
-        var token = buf.toString('hex');
-        done(err, token);
-      });
-    },
-    function(token, done) {
-      User.findOne({ email: req.body.email }, function(err, user) {
-        if (!user) {
-        console.log('error', 'No account with that email address exists.');
-     //     return res.redirect('/forgot');
-        }
-
-        user.resetPasswordToken = token;
-        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-
-        user.save(function(err) {
-          done(err, token, user);
-        });
-      });
-    },
-    function(token, user, done) {
-      var smtpTransport = nodemailer.createTransport({
-        service: 'Gmail', 
-        auth: {
-          user: 'noreply.iclbmsce@gmail.com',
-          pass: process.env.MAIL_KEY
-        }
-      });
-      var mailOptions = {
-        to: user.email,
-        from: 'noreply.iclbmsce@gmail.com',
-        subject: 'Node.js Password Reset',
-        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://' + req.headers.host + '/user/reset/' + token + '\n\n' +
-          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-      };
-      smtpTransport.sendMail(mailOptions, function(err) {
-        console.log('mail sent');
-        console.log('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-        done(err, 'done');
-      });
-    }
-  ], function(err) {
-    if (err) return next(err);
-    console.log("dfjdgfjkhjg")
-   // res.redirect('/forgot');
-  });
-});
-/*
-router.get('reset/:token', function(req, res) {
-  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-    if (!user) {
-      console.log('error', 'Password reset token is invalid or has expired.');
-      //return res.redirect('/forgot');
-    }
-   // res.render('reset', {token: req.params.token});
-  });
-});
-*/
-
-router.post('reset/:token', function(req, res) {
-  async.waterfall([
-    function(done) {
-      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-        if (!user) {
-          console.log('error', 'Password reset token is invalid or has expired.');
-          return res.redirect('back');
-        }
-       
-        if(req.body.password === req.body.confirm) {
-          User.setPassword(req.body.password, function(err) {
-            user.resetPasswordToken = undefined;
-            user.resetPasswordExpires = undefined;
-            user.save(function(err) {
-              req.logIn(user, function(err) {
-                done(err, user);
-              });
-            });
-          })
-
-                  
+      User.findOne({
+        email: req.body.email
+      }).exec(function(err, user) {
+        if (user) {
+          done(err, user);
         } else {
-          console.log("error", "Passwords do not match.");
-         //   return res.redirect('back');
+          done('User not found.');
         }
       });
     },
     function(user, done) {
-      var smtpTransport = nodemailer.createTransport({
-        service: 'Gmail', 
-        auth: {
-          user: 'noreply.iclbmsce@gmail.com',
-          pass: process.env.MAIL_KEY
-        }
+      // create the random token
+      crypto.randomBytes(20, function(err, buffer) {
+        const token = buffer.toString('hex');
+        done(err, user, token);
       });
-      var mailOptions = {
+    },
+    function(user, token, done) {
+      User.findByIdAndUpdate({ _id: user._id }, { reset_password_token: token, reset_password_expires: Date.now() + 86400000 }, { upsert: true, new: true }).exec(function(err, new_user) {
+        done(err, token, new_user);
+      });
+    },
+    function(token, user, done) {
+      var data = {
         to: user.email,
-        from: 'noreply.iclbmsce@gmail.com',
-        subject: 'Your password has been changed',
-        text: 'Hello,\n\n' +
-          'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+        from: email,
+
+        subject: 'Password help has arrived!',
+        text: 'Click to reset Token :  http://localhost:3000/user/reset/' + token
+        
       };
-      smtpTransport.sendMail(mailOptions, function(err) {
-        console.log('success', 'Success! Your password has been changed.');
-        done(err);
+     console.log(token);
+      
+      //console.log("to  : "+this.test)
+      smtpTransport.sendMail(data, function(err) {
+        if (!err) {
+          return res.json({ message: 'Kindly check your email for further instructions' });
+        } else {
+          return done(err);
+        }
       });
     }
   ], function(err) {
-    //res.redirect('/campgrounds');
+    return res.status(422).json({ message: err });
   });
-});
+  
+}
+
+);
 
 
+/**
+ * Reset password
+ */
 
+
+router.post("/reset/:token", (req, res, next) => {
+  User.findOne({
+    reset_password_token: req.params.token,
+    reset_password_expires: {
+      $gt: Date.now()
+    }
+  }
+)
+  .exec(function(err, user) {
+    if (!err &&user) {
+      
+      if (req.body.newPassword === req.body.verifyPassword) {
+        
+        user.password = bcrypt.hashSync(req.body.newPassword, 10);
+        user.reset_password_token = undefined;
+        user.reset_password_expires = undefined;
+        user.save(function(err) {
+          if (err) {
+            return res.status(422).send({
+              message: err
+            });
+          } else {
+            var data = {
+              to: user.email,
+              from: email,
+          
+              subject: 'Password Reset Confirmation',
+              context: {
+             
+              }
+            };
+
+            smtpTransport.sendMail(data, function(err) {
+              if (!err) {
+                return res.json({ message: 'Password reset' });
+              } else {
+                return done(err);
+              }
+            });
+          }
+        });
+      } else {
+        return res.status(422).send({
+          message: 'Passwords do not match'
+        });
+      }
+    } else {
+      
+      
+      return res.status(400).send({
+        
+        message: 'Password reset token is invalid or has expired.'
+      });
+    }
+  });
+})
 module.exports = router;
+
+
+
