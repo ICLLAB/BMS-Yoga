f4e84402002f5b0134021afa9e561675c2e501d7
Sanjana Hathwar <hathwar.sanjana@gmail.com>
2018-10-11 23:53:24 +0530

    TRAINER-DASHBOARD
    Dashboard

---
+++ TRAINER-WORKING1/server.js
@@ -0,0 +1,19 @@
+var express=require('express');
+var nodemailer = require("nodemailer");
+var app=express();
+
+
+ 
+app.get('/',function(req,res){
+    //res.sendfile('Public/index.html');
+    path = require('path');
+            res.sendFile('index.html', { root: path.join(__dirname, './Public') });
+    });
+
+
+var port = process.env.PORT || 5000;
+/*--------------------Routing Over----------------------------*/
+app.use(express.static('Public'));
+app.listen(port,function(){
+console.log("Express Started on Port 5000");
+}); 
